18c2078bfcbba49721f1a8492469d170733e551a
Guillaume Malbrand <gmalbrand@free.fr>
2022-10-21 18:34:33 +0200

    Removing scalyr-agent deployment

--- GKE/create_cluster.sh
+++ GKE/create_cluster.sh
@@ -11,14 +11,3 @@ gcloud container clusters create $CLUSTER_NAME --project="$(gcloud config get pr
 
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials "$CLUSTER_NAME" --project="$(gcloud config get project)" --zone="$(gcloud config get compute/zone)"
-
-
-kubectl create namespace scalyr
-kubectl create secret generic scalyr-api-key --namespace=scalyr --from-literal=scalyr-api-key="0vxrcQGLmk57G9dQamhVqlp4/HjI2/aLh63rTLXSGNVU-"
-
-curl -s https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/scalyr-agent-2-configmap.yaml | \
-  sed "s/<your_cluster_name>/$CLUSTER_NAME/" | \
-  kubectl create -f -
-
-kubectl apply -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/no-kustomize/scalyr-service-account.yaml
-kubectl apply -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/no-kustomize/scalyr-agent-2.yaml
